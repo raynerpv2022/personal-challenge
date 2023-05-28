53c691ba2f2d8ad113d0c58f77c7d4d721e32744
resbaloso <raynerpv2022@gamil.com>
2023-05-28 03:50:28 -0400

    In test_anagram.py, adding comment for subTest function in test_anagram and be clear, what is the argumente i=i? also uncomment test_palindrom function

--- python/test_anagram.py
+++ python/test_anagram.py
@@ -3,13 +3,15 @@ import anagram
 import unittest
 
 class TestPalindrom(unittest.TestCase):
-    # def test_palindrom(self):
-    #     str1 = "Was it a car or a cat I saw"
-    #     str2 = "Was it a car or a cat I saw"
-    #     self.assertTrue(anagram.palindrom(str1,str2))
+    def test_palindrom(self):
+        str1 = "Was it a car or a cat I saw"
+        str2 = "Was it a car or a cat I saw"
+        self.assertTrue(anagram.palindrom(str1,str2))
+
     def test_anagram(self):
         str1 =["zorra","rosa","ramo"]
-        str2 = ["arroz","osar","amors"]
+        str2 = ["arroz","osar","amor"]
+        # for loop to test several case, introduccing subTest function, here i dont know  what is i=i argumnnet
         for i in range(len(str1)):
             with self.subTest(i=i):
                 self.assertTrue(anagram.anagram(str1[i],str2[i]))
